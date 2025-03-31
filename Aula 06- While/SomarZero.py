4b47a51cb5376fffeac0c09eb3bd2f1bd585309f
AmandhaYumi <mandhayumi@gmail.com>
2025-03-31 13:20:32 -0300

    Adicionando aula 5 e 6

---
+++ Aula 06- While/SomarZero.py
@@ -0,0 +1,10 @@
+#---------------------------------------------------------------------
+
+#Somar zero
+x = True
+y = 0
+while x:
+    x = int(input("Digite um número: "))
+    y += x
+
+print(f"Y: {y}")
---
+++ Aula 06- While/Tabuada.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------
+
+#Tabuada
+x = int(input("Digite um número: "))
+i = 1
+while(i <= 10):
+    print(f"{x} X {i:2d} = {(x * i):2d}")
+    i += 1
+
+
+#for i in range(1, 11, 1):
+#    print(f"{x} X {i} = {x * i}")
+
